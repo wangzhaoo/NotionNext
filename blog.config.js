7c33a12f948be2915a97d7ace0364b1a26e8ec8b
wangzhaoo <73371737+wangzhaoo@users.noreply.github.com>
2022-04-22 15:15:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -43,10 +43,10 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com',
+  CONTACT_EMAIL: '980540247@qq.com',
   CONTACT_WEIBO: '',
   CONTACT_TWITTER: '',
-  CONTACT_GITHUB: 'https://github.com/tangly1024',
+  CONTACT_GITHUB: 'https://github.com/wangzhaoo',
   CONTACT_TELEGRAM: '',
 
   // 悬浮挂件
